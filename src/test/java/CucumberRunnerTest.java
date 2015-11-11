1c794b176f652a4e75b0b5f33c5eb4caefdef491
Imtiaz Hossain <imtihoss@gmail.com>
2015-11-10 22:02:19 -0500

    change output to html

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        format = {"pretty", "html:cucumber-html-reports"},
+        format = {"pretty", "html:target"},
         features = "src/test/resources"
 )
 public class CucumberRunnerTest {
